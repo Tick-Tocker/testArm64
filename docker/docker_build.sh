f096cc9b334b89ece73bd2f17ed8c96c6fcb0291
Jingzhao.Ni <Jingzhao.Ni@arm.com>
2020-01-17 15:03:16 +0800

    Fix bugs
    Signed-off-by: Jingzhao.Ni <Jingzhao.Ni@arm.com>

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -3,4 +3,4 @@
 LINUX_DISTRO="ubuntu"
 IMAGE_NAME=test-build-"${LINUX_DISTRO}"
 
-docker build -f Dockerfile-${LINUX_DISTRO} -t ${IMAGE_NAME}:latest .
+docker build -f docker/Dockerfile -t ${IMAGE_NAME}:latest ./docker
