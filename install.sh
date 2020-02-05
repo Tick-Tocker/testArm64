4340edf8203232d0022682c8690d2eebe5bbea0d
Jingzhao.Ni <Jingzhao.Ni@arm.com>
2020-02-05 14:18:40 +0000

    Test 2
    Test 2
    
    Signed-off-by: Jingzhao.Ni <Jingzhao.Ni@arm.com>

--- install.sh
+++ install.sh
@@ -9,6 +9,17 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 docker version
 docker manifest --help
 
+echo 111111111111111111111111111111111111111111
+
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/microsoft-prod.list
+apt-get update
+apt-get install -y moby-engine moby-cli
+
+
+
 ls /etc/docker/
 cat /etc/docker/daemon.json
 
