d36fede05930034e6c0da1a51134b141adc968ea
Jingzhao.Ni <Jingzhao.Ni@arm.com>
2020-02-05 14:01:07 +0000

    Test 13
    Test 13
    
    Signed-off-by: Jingzhao.Ni <Jingzhao.Ni@arm.com>

--- install.sh
+++ install.sh
@@ -5,4 +5,15 @@ if [ "$(uname -m)" == "aarch64" ]; then
 else
   echo "this is amd64 functions"
 fi
+export DOCKER_CLI_EXPERIMENTAL=enabled
+docker version
+docker manifest --help
 
+
+cat /etc/docker/daemon.json
+
+echo $'{\n    "experimental": true\n}' | tee /etc/docker/daemon.json
+service docker restart
+
+docker manifest --help
+echo 999999999999999999999
