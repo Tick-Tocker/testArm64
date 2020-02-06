111ab13e84a02f74b7a2416209b29ed09575c71b
Jingzhao.Ni <Jingzhao.Ni@arm.com>
2020-02-06 06:40:21 +0000

    testtest
    testtest
    
    Signed-off-by: Jingzhao.Ni <Jingzhao.Ni@arm.com>

--- install.sh
+++ install.sh
@@ -1,17 +1,3 @@
 #!/bin/bash -x
 
-if [ "$(uname -m)" == "aarch64" ]; then
-  echo "this is aarch64 platform"
-else
-  echo "this is amd64 functions"
-fi
-
-export DOCKER_CLI_EXPERIMENTAL=enabled
-docker version
-docker manifest --help
-
-echo 111111111111111111111111111111111111111111
-
-docker manifest create --help
-echo 99999999999999999999999999999999
 source docker_push.sh
